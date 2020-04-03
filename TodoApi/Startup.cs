f11184b7dfb5bbb879822a1f05badd4cfca5870e
hannesottt <hannesottt@gmail.com>
2020-04-03 06:31:29 +0300

    javascript

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -39,6 +39,10 @@ namespace TodoApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
